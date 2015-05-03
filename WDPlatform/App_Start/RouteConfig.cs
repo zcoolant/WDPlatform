648156642f47ccbb8028e4ca9bccdccf3b0084ca
Tong Zhao <tong.zhao@kaseya.com>
2015-05-03 15:34:31 -0400

    try to run

--- WDPlatform/App_Start/RouteConfig.cs
+++ WDPlatform/App_Start/RouteConfig.cs
@@ -15,7 +15,7 @@ namespace WDPlatform
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
+                url: "{*anything}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
